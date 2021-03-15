8139baf7a5fa2a2fd17d4e6b2484753a6e997bb4
MT-cod <mammmoth@list.ru>
2021-03-15 22:15:09 +0300

    4

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ validate:
 	composer validate
 lint:
 	composer run-script phpcs -- --standard=PSR12 bin tests
+test:
+	composer exec --verbose phpunit tests
---
+++ tests/GenDiffTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Projects\lvl2;
+
+use PHPUnit\Framework\TestCase;
+
+class GenDiffTest extends TestCase
+{
+    public $rightResult;
+    public $runDiffResult;
+
+    public function testGenDiff()
+    {
+        $this->rightResult = '{
+- follow: false
+  host: hexlet.io
+- proxy: 123.234.53.22
+- timeout: 50
++ timeout: 20
++ verbose: true
+}
+';
+        $this->runDiffResult = shell_exec(__DIR__ . '/../bin/gendiff '.__DIR__.'/fixtures/file1.json '.__DIR__.'/fixtures/file2.json');
+        $this->assertEquals($this->rightResult, $this->runDiffResult);
+    }
+}
